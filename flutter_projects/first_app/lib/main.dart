6b651f5d98254aefcbe6620815bcbdc30b97ff16
eunjaelim <imeunjae361@gmail.com>
2025-01-20 20:38:45 +0900

    gradient / begin / end

--- flutter_projects/first_app/lib/main.dart
+++ flutter_projects/first_app/lib/main.dart
@@ -11,6 +11,8 @@ void main() {
                 Color.fromARGB(255, 81, 41, 150),
                 Color.fromARGB(255, 109, 88, 145),
               ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
           ),
           child: const Center(child: Text('Hello World!')),
