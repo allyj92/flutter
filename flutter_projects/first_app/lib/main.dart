06e5c5fac4c723bff1974317041ad7adf160996e
eunjaelim <imeunjae361@gmail.com>
2025-02-18 22:07:18 +0900

    dice repair errors

--- flutter_projects/first_app/lib/main.dart
+++ flutter_projects/first_app/lib/main.dart
@@ -2,14 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:first_app/gradient_container.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         body: GradientContainer(
           Colors.deepPurple,
           const Color.fromARGB(255, 69, 38, 122),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
