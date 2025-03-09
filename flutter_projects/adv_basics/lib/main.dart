f5c69afea32247fdccf16ee2aae5f909d58bf49b
eunjaelim <imeunjae361@gmail.com>
2025-03-09 15:11:09 +0900

    Update StartScreen: Add image, text, and OutlinedButton

---
+++ flutter_projects/adv_basics/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 73, 23, 160),
+                Color.fromARGB(255, 99, 16, 124),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
